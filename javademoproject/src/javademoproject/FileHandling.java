e9bb9620277cb2605b5e9b34cc12e3e584d5d2fb
Harshit Mishra <harmishra@BLR.ALLEGISINDIA.COM>
2023-02-28 12:55:29 +0530

    changes for write in file handling

--- javademoproject/src/javademoproject/FileHandling.java
+++ javademoproject/src/javademoproject/FileHandling.java
@@ -3,7 +3,8 @@ package javademoproject;
 import java.io.*;
 public class FileHandling {
 public static void main(String[] args) {
-	fileInputStreamDemo();
+	//fileInputStreamDemo();
+	fileWriterDemo();
 }
 static void fileInputStreamDemo() {
 	int i;
@@ -20,5 +21,21 @@ static void fileInputStreamDemo() {
 	} catch (IOException e) {
 		System.out.println("exception occured "+e.getMessage());
 	}
+}
+static  void fileWriterDemo()
+{
+	File myfile=new File("C:\\Users\\harmishra\\Documents\\check.txt");
+	FileWriter fis;
+	try {
+		fis = new FileWriter(myfile);
+		System.out.println("file opened");
+		fis.write("writing file through file handling \n");
+		fis.append("some more words");
+		fis.close();
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
+	
+	
 }
 }
